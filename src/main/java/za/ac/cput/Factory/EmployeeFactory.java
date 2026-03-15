fe534159a42a03ab54ff4c9f3ec59e81e130290d
Moltinate230074189 <230074189@mycput.ac.za>
2026-03-15 14:15:42 +0200

    Added EmployeeFactory

--- src/main/java/za/ac/cput/Factory/EmployeeFactory.java
+++ src/main/java/za/ac/cput/Factory/EmployeeFactory.java
@@ -1,4 +1,27 @@
 package za.ac.cput.Factory;
 
+import za.ac.cput.Domain.Employee;
+import za.ac.cput.Util.Email;
+import java.util.UUID;
+
 public class EmployeeFactory {
+
+    public static Employee createEmployee(String firstName, String lastName, String emailAddress) {
+
+        if(firstName == null || firstName.isEmpty())
+            return null;
+        if(lastName == null || lastName.isEmpty())
+            return null;
+        if(!Email.isValidEmail(emailAddress))
+            return null;
+
+        String customerId = UUID.randomUUID().toString();
+
+        return new Employee.Builder()
+                .setEmployeeId(employeeId)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmailAddress(emailAddress)
+                .build();
+    }
 }
