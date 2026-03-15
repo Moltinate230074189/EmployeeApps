1278e3bddb7b360b67580cc9af1f15ffe032bca1
Moltinate230074189 <230074189@mycput.ac.za>
2026-03-15 14:16:57 +0200

    Added Email

--- src/main/java/za/ac/cput/Util/Email.java
+++ src/main/java/za/ac/cput/Util/Email.java
@@ -1,4 +1,8 @@
 package za.ac.cput.Util;
 
 public class Email {
+
+    public static boolean isValidEmail(String email) {
+        return email != null && email.matches("^[A-Za-z0-9+_.-]+@(.+)$");
+    }
 }
