2de3c8f5a56ec8a72341141e257ce5f430f39cc6
Moltinate230074189 <230074189@mycput.ac.za>
2026-03-15 14:05:18 +0200

    Added EmployeeIdFactory

--- src/main/java/za/ac/cput/Domain/EmployeeIdGenerator.java
+++ src/main/java/za/ac/cput/Domain/EmployeeIdGenerator.java
@@ -1,4 +1,23 @@
 package za.ac.cput.Domain;
 
 public class EmployeeIdGenerator {
+
+    private static EmployeeIdGenerator instance;
+    private int currentId;
+
+    private EmployeeIdGenerator (int currentId) {
+        this.currentId = currentId;
+    }
+
+    public static EmployeeIdGenerator getInstance() {
+        if (instance == null) {
+            instance = new EmployeeIdGenerator (1000);
+        }
+
+        return instance;
+    }
+
+    public int generateId() {
+        return ++ currentId;
+    }
 }
